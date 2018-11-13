53f3da46eaca3eadcca446c9a1cc878201e8c4c7
Greg Roques <gregroques@Gregs-MacBook-Pro.local>
2018-11-13 12:05:29 -0500

    Second attemt at commit

---
+++ Python101.py
@@ -0,0 +1,25 @@
+print "Hello, World";
+
+print """
+Hello
+    World!
+""";
+
+# A variable is just a fast way to refer to something else
+
+theBestClass = "the 11-18 immersive"
+print theBestClass;
+
+# Data Types
+# - Programming languages see different types of variable differently.
+print 3+3;
+# Strings
+# Numbers
+# Booleans - true or false, off or on, 1 or 0...binary choices
+# lists - [a, b, c, d, ..., etc.]... same as an array
+# Dictionaries - variable of variables
+# Objects - Super dictionaries
+
+# Primative Data Types: String, Number, Boolean
+
+
