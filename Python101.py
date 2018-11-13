036c03d1f1a139ecff1ee1c370ffa2c838ffd721
Greg Roques <gregroques@Gregs-MacBook-Pro.local>
2018-11-13 14:28:14 -0500

    Add operators

--- Python101.py
+++ Python101.py
@@ -1,40 +1,77 @@
-print "Hello, World";
+# print "Hello, World";
 
-print """
-Hello
-    World!
-""";
+# print """
+# Hello
+#     World!
+# """;
 
-# A variable is just a fast way to refer to something else
+# # A variable is just a fast way to refer to something else
 
-theBestClass = "the 11-18 immersive"
-print theBestClass;
+# theBestClass = "the 11-18 immersive"
+# print theBestClass;
 
-# Data Types
-# - Programming languages see different types of variable differently.
-print 3+3;
-# Strings
-# Numbers
-# Booleans - true or false, off or on, 1 or 0...binary choices
-# lists - [a, b, c, d, ..., etc.]... same as an array
-# Dictionaries - variable of variables
-# Objects - Super dictionaries
+# # Data Types
+# # - Programming languages see different types of variable differently.
+# print 3+3;
+# # Strings
+# # Numbers
+# # Booleans - true or false, off or on, 1 or 0...binary choices
+# # lists - [a, b, c, d, ..., etc.]... same as an array
+# # Dictionaries - variable of variables
+# # Objects - Super dictionaries
 
-# Primative Data Types: String, Number, Boolean
+# # Primative Data Types: String, Number, Boolean
 
-month = "November";
-print type (month);
-date = 13
-print type(date)
-date2 = 13.008
-print type(date2)
+# month = "November";
+# print type (month);
+# date = 13
+# print type(date)
+# date2 = 13.008
+# print type(date2)
 
-aBool = True;
-print type(aBool)
+# aBool = True;
+# print type(aBool)
 
-aList = [];
-print type(aList)
+# aList = [];
+# print type(aList)
 
-aDictionary = {};
-print type(aDictionary)
+# aDictionary = {};
+# print type(aDictionary)
+
+# # concatenate is prgramming speak for add things together
+# first = "Greg"
+# last = "Roques"
+# fullName = first + " " + last; 
+# print fullName;
+
+# fourteen = 10 + 4;
+# print fourteen
+# oneOhFour = "10" + "4"
+# print oneOhFour;
+
+# # cast = change a variable to a new data type;
+
+# fourteen2 = int("10") + 4;
+
+# print 2+2;
+# print 2-2;
+# print 2*2;
+# print 2/2;
+# print 2%2;
+
+# % = Modulas. This divides the number and gives you the remainder. 
+
+print 2%3;
+
+print 2**3; # 2 to the power of ^3;
+
+print "-" * 20 # will print 20 dashes; re: in this case, give me 20 of something;
+
+# Python does not have a simple incrementer.
+
+# num++ does not work in Python.
+
+num += 1
+
+
 
